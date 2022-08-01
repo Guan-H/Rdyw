a671ab5bd5a95b63770926838ceca8d801a4d758
Guan-H <1421938824@qq.com>
2022-08-01 17:46:01 +0800

    20220801_修改了axios请求格式

--- src/main/java/com/gzcss/Rdyw/controller/RdywController.java
+++ src/main/java/com/gzcss/Rdyw/controller/RdywController.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController()
@@ -15,27 +17,21 @@ import javax.servlet.http.HttpServletRequest;
 public class RdywController {
 
     @PostMapping("rdyw")
-    public RdywEntity Rdyw(HttpServletRequest request, @RequestBody RdywEntity rdywEntity){
-        RdywEntity rdywEntity1 = new RdywEntity();
-        rdywEntity1.setReport_Arch(rdywEntity.getReport_Arch());
-        rdywEntity1.setReport_CPUs(rdywEntity.getReport_CPUs());
-        rdywEntity1.setReport_CPUType(rdywEntity.getReport_CPUType());
-        rdywEntity1.setReport_DiskFree(rdywEntity.getReport_DiskFree());
+    public void Rdyw(HttpServletRequest request, @RequestBody RdywEntity rdywEntity){
+        log.info("接收到的数据{}",rdywEntity);
 
-        log.info(rdywEntity.toString());
-
-        request.getSession().setAttribute("rdywEntity",rdywEntity1);
-        return  rdywEntity1;
     };
 
     @ResponseBody
     @GetMapping("test")
-    public Test test(){
+    public List<Test> test(){
         Test test= new Test();
         test.setAddress("非洲");
         test.setName("大傻逼");
         test.setDate("2022-03-03");
-        return test;
+        List<Test> list = new ArrayList<>();
+        list.add(test);
+        return list;
     }
 
 }
--- src/main/java/com/gzcss/Rdyw/entity/RdywEntity.java
+++ src/main/java/com/gzcss/Rdyw/entity/RdywEntity.java
@@ -5,15 +5,15 @@ import lombok.Data;
 @Data
 public class RdywEntity {
 
-    private String report_CPUs="";    //CPU数量
-    private String report_CPUType="";    //CPU类型
-    private String report_Arch="";    //CPU架构
-    private String report_MemTotal="";    //内存总容量(MB)
-    private String report_MemFree="";    //内存剩余(MB)
-    private String report_MemUsedPercent="";    //内存使用率%
-    private String report_DiskTotal="";    //硬盘总容量(GB)
-    private String report_DiskFree="";    //硬盘剩余(GB)
-    private String report_DiskUsedPercent="";    //硬盘使用率%
+    private String reportCPUs;    //CPU数量
+    private String reportCPUType;    //CPU类型
+    private String reportArch;    //CPU架构
+    private String reportMemTotal;    //内存总容量(MB)
+    private String reportMemFree;    //内存剩余(MB)
+    private String reportMemUsedPercent;    //内存使用率%
+    private String reportDiskTotal;    //硬盘总容量(GB)
+    private String reportDiskFree;    //硬盘剩余(GB)
+    private String reportDiskUsedPercent;    //硬盘使用率%
 
 
 }
